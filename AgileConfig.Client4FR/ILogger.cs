c6d9ea2bec30f45eb24cd9b93f57b210f69e0d1a
mjzhou <kklldog@qq.com>
2021-11-11 13:47:32 +0800

    添加项目文件。

---
+++ AgileConfig.Client4FR/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgileConfig.Client
+{
+    public interface ILogger
+    {
+        void LogError(string msg);
+        void LogError(Exception ex, string msg);
+        void LogTrace(string msg);
+
+    }
+}
